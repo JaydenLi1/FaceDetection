b9496ed2769b51eee4e4fc271f6fef6c58a98de1
Jayden Li <126529751+JaydenLi1@users.noreply.github.com>
2024-03-09 20:02:57 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./screens/main";
+
+export default function App() {
+  return <Main />;
+}
